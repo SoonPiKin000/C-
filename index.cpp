75d2f8baf543e243612dfc634f276b191636e59f
unknown <soonpikin@gmail.com>
2024-08-11 00:01:19 +0700

    bai1

---
+++ index.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    int a[n];
+    // Nhập mảng
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    // Đẩy mảng vào set
+    set<int> se;
+    for (int i = 0; i < n; i++)
+    {
+        se.insert(a[i]);
+    }
+    cout << se.size() << endl;
+    return 0;
+}
