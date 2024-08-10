de24cc94924bd96ac2b908dcdaee9e21b08c2009
unknown <soonpikin@gmail.com>
2024-08-11 00:32:28 +0700

    bai 2

--- index.cpp
+++ index.cpp
@@ -12,18 +12,29 @@ int main()
     cin >> n;
     int a[n];
     // Nhập mảng
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    // Đẩy mảng vào set
     set<int> se;
     for (int i = 0; i < n; i++)
     {
+        cin >> a[i];
         se.insert(a[i]);
     }
-    for(int x : se){
-        cout << x << " ";
+    int b;
+    cin >> b;
+    string kq[b]; 
+    for(int i = 0; i < b; i++){
+        int c; 
+        cin >> c; 
+        auto it = se.find(c);
+        if(it == se.end()){
+            kq[i] = "NO\n";
+        }
+        else{
+            kq[i] = "YES\n";
+        }
+    }
+    // Đẩy mảng vào set
+    for(int i = 0; i < b; i++){
+        cout << kq[i] ; 
     }
     return 0;
 }
